05bd76c7601b52b00a7890d62d7a65c6507ac9e5
borgesenioc <borgesenioc@gmail.com>
2024-06-15 12:10:44 -0300

    Add GET and POST methods for evelope objects

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # personal-budget
+
+The idea here is to project a monthly income, deduct projected expenses, and then forecast how much balance would be left by the end of the month.
+
+### Budget structure:
+Monthly income, Envelope spending, Unbudgeted expenses, Projected taxes, and Balance Left.
+
+### Budget variables:
+id, value
+
+### Envelopes: 
+Housing, Eating out, Groceries, Savings, Pet care, Online purchases, Transportation, Insurance, and Health spending 
+
+### Envelope variables:
+id, name, value
+
+
--- app.js
+++ app.js
@@ -2,9 +2,51 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+// Global variables to store information about envelopes and total budget
+let envelopes = [];
+let totalBudget = 0;
+
+// Function to generate unique ID for envelopes
+const  generateID = () => {
+    return (envelopes.lenght) ? envelopes[envelopes.lenght - 1].id + 1 : 1;
+};
+
+// Endpoint to create a new budget envelope
+app.post('/envelopes', (req, res, next) => {
+    const { name, value } = req.body;
+
+    if (!name || value === undefined) {
+        return res.status(400).json({ error: 'Name and value are required' });
+    }
+
+    const newEnvelope = {
+        id: generateID(),
+        name: name,
+        value: value
+    };
+
+    envelopes.push(newEnvelope);
+    totalBudget += value;
+
+    res.status(201).json(newEnvelope);
+});
+
+// Endpoint to get all envelopes
+app.get('/envelopes', (req, res, next) => {
+    res.status(200).json(envelopes);
+});
+
+app.get('/total-budget', (req, res, next) => {
+    res.status(200).json(totalBudget);
+});
+
+/*app.get('/', (req, res) => {
     res.send("Hello, World");
-})
+})*/
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
