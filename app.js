2db35bff6130d30fd3c6b25c9068f037d987607f
borgesenioc <borgesenioc@gmail.com>
2024-06-17 09:38:04 -0300

    Add method to delete an envelope by id

--- app.js
+++ app.js
@@ -9,96 +9,112 @@ let envelopes = [];
 let totalBudget = 0;
 
 // Function to generate unique ID for envelopes
-const  generateID = () => {
-    return (envelopes.length) ? envelopes[envelopes.length - 1].id + 1 : 1;
+const generateID = () => {
+  return envelopes.length ? envelopes[envelopes.length - 1].id + 1 : 1;
 };
 
 // Function to get an envelope by the ID
 const getEnvelopeById = (id) => {
-    return envelopes.find(envelope => envelope.id === parseInt(id));
-  };  
+  return envelopes.find(envelope => envelope.id === parseInt(id));
+};
 
 // Function to deposit or withdraw from an envelope
 const withdrawOrDeposit = (num, str) => {
-    if (str === 'withdraw') {
-        num = -num;
-    }
-    return num;
+  if (str === 'withdraw') {
+    num = -num;
+  }
+  return num;
+};
+
+// Function to delete an envelope by ID
+const deleteEnvelopeById = (id) => {
+  const index = envelopes.findIndex(envelope => envelope.id === parseInt(id));
+  if (index !== -1) {
+    const deletedEnvelope = envelopes.splice(index, 1)[0];
+    totalBudget -= deletedEnvelope.value; // Adjust total budget
+    return deletedEnvelope;
+  }
+  return null;
 };
 
 // Endpoint to create a new budget envelope
-app.post('/envelopes', (req, res, next) => {
-    const { name, value } = req.body;
+app.post('/envelopes', (req, res) => {
+  const { name, value } = req.body;
 
-    if (!name || value === undefined) {
-        return res.status(400).json({ error: 'Name and value are required' });
-    }
+  if (!name || value === undefined) {
+    return res.status(400).json({ error: 'Name and value are required' });
+  }
 
-    const newEnvelope = {
-        id: generateID(),
-        name: name,
-        value: value
-    };
+  const newEnvelope = {
+    id: generateID(),
+    name: name,
+    value: value
+  };
 
-    envelopes.push(newEnvelope);
-    totalBudget += value;
+  envelopes.push(newEnvelope);
+  totalBudget += value;
 
-    res.status(200).json(newEnvelope);
+  res.status(201).json(newEnvelope);
 });
 
 // Endpoint to get all envelopes
-app.get('/envelopes', (req, res, next) => {
-    res.status(200).json(envelopes);
+app.get('/envelopes', (req, res) => {
+  res.status(200).json(envelopes);
 });
 
-app.get('/total-budget', (req, res, next) => {
-    res.status(200).json(totalBudget);
+// Endpoint to get total budget
+app.get('/total-budget', (req, res) => {
+  res.status(200).json({ totalBudget });
 });
 
 // Endpoint to get a specific envelope
-app.get('/envelopes/:id', (req, res, next) => {
-    const envelopeId = req.params.id;
-    const findEnvelope = getEnvelopeById(envelopeId);
+app.get('/envelopes/:id', (req, res) => {
+  const envelopeId = req.params.id;
+  const findEnvelope = getEnvelopeById(envelopeId);
 
-    if (findEnvelope) { 
-        res.status(200).json(findEnvelope);
-    } else {
-        res.status(404).send(`Envelope ID ${envelopeId} does not exist.`);
-    };
-    
+  if (findEnvelope) {
+    res.status(200).json(findEnvelope);
+  } else {
+    res.status(404).send(`Envelope ID ${envelopeId} does not exist.`);
+  }
 });
 
-// Endpoint to upsdate a specific envelope
-app.put('/envelopes/:id', (req, res, next) =>  {
-    const envelopeId = req.params.id;
-    const findEnvelope = getEnvelopeById(envelopeId);
+// Endpoint to update a specific envelope
+app.put('/envelopes/:id', (req, res) => {
+  const envelopeId = req.params.id;
+  const findEnvelope = getEnvelopeById(envelopeId);
 
-    if (findEnvelope) {
-        const { name, value, transaction_type } = req.body;
+  if (findEnvelope) {
+    const { name, value, transaction_type } = req.body;
 
-        if (value !== undefined && transaction_type) {
-          const amount = withdrawOrDeposit(value, transaction_type);
-          findEnvelope.value += amount;
-          totalBudget += amount;
-        }
-    
+    if (value !== undefined && transaction_type) {
+      const amount = withdrawOrDeposit(value, transaction_type);
+      findEnvelope.value += amount;
+      totalBudget += amount;
+    }
+
     if (name) {
-        findEnvelope.name = name;
+      findEnvelope.name = name;
     }
 
     res.status(200).send(`Envelope ${findEnvelope.name}'s current balance is ${findEnvelope.value}`);
-    } else {
-        res.status(404). send(`Envelope ID ${envelopeID} does not exist.`)
-    };
-
+  } else {
+    res.status(404).send(`Envelope ID ${envelopeId} does not exist.`);
+  }
 });
 
-/*app.get('/', (req, res) => {
-    res.send("Hello, World");
-})*/
+// Endpoint to delete a specific envelope
+app.delete('/envelopes/:id', (req, res) => {
+  const envelopeId = req.params.id;
+  const deletedEnvelope = deleteEnvelopeById(envelopeId);
 
-
+  if (deletedEnvelope) {
+    res.status(200).send(`Envelope ID ${envelopeId} was deleted.`);
+  } else {
+    res.status(404).send(`Envelope ID ${envelopeId} does not exist.`);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
